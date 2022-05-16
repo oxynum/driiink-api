f36b8f01dffb48654468ee0d20a2ead779f72eed
Elyes Voisin <voisin.elyes@hotmail.com>
2022-05-16 10:38:12 +0200

    FIX auth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,16 +12,31 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            // parameters usually defined in Symfony login forms
+            'error' => $error,
+            'last_username' => $lastUsername,
+
+            'translation_domain' => 'admin',
+            'page_title' => 'DRIIINK ADMIN',
+            'csrf_token_intention' => 'authenticate',
+            'target_path' => $this->generateUrl('admin'),
+            'username_label' => 'Your email',
+            'password_label' => 'Your password',
+            'sign_in_label' => 'Log in',
+            'username_parameter' => 'email',
+            'password_parameter' => 'password',
+            'forgot_password_enabled' => false,
+            'forgot_password_path' => $this->generateUrl('app_login'),
+            'forgot_password_label' => 'Forgot your password?',
+            'remember_me_enabled' => true,
+            'remember_me_parameter' => 'custom_remember_me_param',
+            'remember_me_checked' => true,
+            'remember_me_label' => 'Remember me',
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
